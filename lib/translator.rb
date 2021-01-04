f22842c5a961fc2d5c370991adc2f793aa198a2b
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 05:13:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,8 @@ def get_english_meaning (path, emoticon)
   load_library(path).each do |key, value|
     if value[:japanese] == emoticon
       return key
+      if value[:japanese] != emoticon
+        return "Sorry, that emoticon was not found"
     end
   end
 end
