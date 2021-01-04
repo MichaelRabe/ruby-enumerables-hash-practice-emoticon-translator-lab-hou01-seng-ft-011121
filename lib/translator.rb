cd8a40f9114200885356d3b9387f8f2d21194b56
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 05:18:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,12 @@ def load_library(path)
 end
 
 def get_japanese_emoticon(path, emoticon)
-  load_library(path)
+  load_library(path).each do |key, value|
+    binding.pry
+    if value[:english] == emoticon
+      return 
+    end
+  end
 end
 
 def get_english_meaning (path, emoticon)
